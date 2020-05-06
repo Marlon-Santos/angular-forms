e974d32985dc8c47e410937b97f686325b3ab05d
Marlon-Santos <marlonergondossantos@gmail.com>
2020-05-06 15:04:44 -0300

    create form[

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenComponent
   ],
   imports: [
     BrowserModule,
